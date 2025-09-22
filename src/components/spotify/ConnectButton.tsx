87a329dded4a07c33a0e7a3491083f98486e0977
Henry <134236009+braindead-dev@users.noreply.github.com>
2025-09-22 00:51:06 -0700

    Update ConnectButton.tsx

--- src/components/spotify/ConnectButton.tsx
+++ src/components/spotify/ConnectButton.tsx
@@ -2,18 +2,24 @@
 
 import { Button } from "@/components/ui/button";
 import { FaSpotify } from "react-icons/fa";
+import { Attribution } from "@/components/site/attribution";
 
 export function ConnectButton() {
   return (
-    <Button
-      onClick={() => {
-        window.location.href = "/api/spotify/login";
-      }}
-      variant="fancy"
-      className="px-2 py-1"
-    >
-      Connect to Spotify
-      <FaSpotify className="-ml-0.5 text-green-500" />
-    </Button>
+    <>
+      <Button
+        onClick={() => {
+          window.location.href = "/api/spotify/login";
+        }}
+        variant="fancy"
+        className="px-2 py-1"
+      >
+        Connect to Spotify
+        <FaSpotify className="-ml-0.5 text-green-500" />
+      </Button>
+      <div className="absolute right-4 bottom-4">
+        <Attribution />
+      </div>
+    </>
   );
 }
